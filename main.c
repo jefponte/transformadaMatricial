a27fa8010bd5cc6148658ad5214f8349015f1463
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-08-21 11:35:55 -0300

    Maquiada.

--- main.c
+++ main.c
@@ -207,7 +207,7 @@ int main(void)
 		printf("--------------------------------------------------------\n");
 	   	for (linha = 0; linha < 6; linha++) {
 	   		for (coluna = 0; coluna < 1; coluna++) {
-	   			printf(" %.6f \t", a[linha][coluna]);
+	   			printf(" %.6f \t", (-1)*a[linha][coluna]);
 
 	   		}
 	   		printf("\n");
