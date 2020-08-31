b84559b2dd55f6d2ff90df4c93511f2f6f7fc969
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-08-31 14:19:38 -0300

    Multiplicação funcionando.

--- matrix/main.c
+++ matrix/main.c
@@ -6,6 +6,54 @@
  * Faz a transposta da matriz M e põe o resultado em transposta.
  * O número de linhas é trocado pelo número de colunas e o número de colunas é trocado pelo número de linhas.
  */
+void transposta(double *matriz, int linhas, int colunas, double *transposta);
+
+/**
+ *
+ * Realiza a operação de multiplicação de matrizes: AXB e põe o resultado na matriz resultado.
+ * Retorno 0 indica erro, as matrizes devem ser compatíveis para esta operação(O número de colunas da matriz A deve ser igual ao número de linhas da Matriz B).
+ * Os ponteiros para inteiros das linhas e colunas da matriz resultado atualizarão o resultado do número de linhas e colunas da nova matriz.
+ * (A matriz resultado terá o número de linhas de A e o Número de Colunas de B).
+ */
+int multiplica(
+		double *a, int linhasA, int colunasA,
+		double *b, int linhasB, int colunasB,
+		double *resultado, int *linhasR, int *colunasR);
+/**
+ * Calcula inversa de uma matriz quadrada de ordem m
+ */
+int inversa(double *matriz, int ordem, double *resultado);
+
+/**
+ * Percorre e exibe os valores de uma matriz.
+ */
+void mostraMatriz(double *matriz, int linhas, int colunas);
+
+
+int main(void)
+{
+	double a[2][3] = {1,-3, 6, 4, 5, 0};
+	double b[3][2] = {2,4,-1,5,3,6};
+	double resultado[2][2];
+	int linhasR, colunasR;
+
+	printf("\n-------Resultado---------------\n");
+	mostraMatriz(a, 2, 3);
+	printf("\n-------Resultado---------------\n");
+	mostraMatriz(a, 3, 2);
+	printf("\n-------Resultado---------------\n");
+	multiplica(a, 2, 3, b, 3, 2, resultado, &linhasR, &colunasR);
+	mostraMatriz(resultado, linhasR, colunasR);
+
+	return 0;
+}
+
+
+
+/**
+ * Faz a transposta da matriz M e põe o resultado em transposta.
+ * O número de linhas é trocado pelo número de colunas e o número de colunas é trocado pelo número de linhas.
+ */
 void transposta(double *matriz, int linhas, int colunas, double *transposta)
 {
 	int i, j;
@@ -46,7 +94,14 @@ int multiplica(
 
 	return 1;
 }
+/**
+ * Calcula inversa de uma matriz quadrada de ordem m
+ */
+int inversa(double *matriz, int ordem, double *resultado)
+{
 
+	return 1;
+}
 void mostraMatriz(double *matriz, int linhas, int colunas){
 	int i, j;
 	for(i = 0; i < linhas; i++){
@@ -55,22 +110,4 @@ void mostraMatriz(double *matriz, int linhas, int colunas){
 		}
 		printf("\n");
 	}
-}
-
-int main(void)
-{
-	double a[2][3] = {1,-3, 6, 4, 5, 0};
-	double b[3][2] = {2,4,-1,5,3,6};
-	double resultado[2][2];
-	int linhasR, colunasR;
-
-	printf("\n-------Resultado---------------\n");
-	mostraMatriz(a, 2, 3);
-	printf("\n-------Resultado---------------\n");
-	mostraMatriz(a, 3, 2);
-	printf("\n-------Resultado---------------\n");
-	multiplica(a, 2, 3, b, 3, 2, resultado, &linhasR, &colunasR);
-	mostraMatriz(resultado, linhasR, colunasR);
-
-	return 0;
 }
