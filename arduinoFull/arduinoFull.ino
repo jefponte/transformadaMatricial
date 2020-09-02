70163a088bc727f614c0f5116f19c412d7d11d18
Jefferson Uchôa Ponte <jefponte@gmail.com>
2020-09-02 11:11:15 -0300

    Aplicação leve e bibliotecas completas.

---
+++ arduinoFull/arduinoFull.ino
@@ -0,0 +1,164 @@
+#include <SoftwareSerial.h>
+
+
+
+void setup()
+{
+  	// Open serial communications and wait for port to open:
+	Serial.begin(115200);
+	while (!Serial) {
+	  ; // wait for serial port to connect. Needed for Native USB only
+	}
+  Serial.println("Sistema em funcionamento. Preparar treinamento da matriz...");
+}
+
+void loop() // run over and over
+{
+	main2();
+	delay(22000);   
+}
+
+  	
+int main2(void)
+{
+  float b[3][3] = {1, 2, 3, 0, 1, 4, 0, 0, 1};
+  float resultado[3][3];
+  mostraMatriz((float*)b, 3, 3);
+  Serial.println("\n----------------\n");
+  inversa((float*)b, 3, (float*)resultado);
+  mostraMatriz((float*)resultado, 3, 3);
+
+	return 0;
+}
+
+void treinar(float* x, float* y, float *a){
+  float tx[100][34];
+  float xTx[34][34];
+  float xTxInv[34][34];
+  float yTx[6][34];
+  int linhasXtx, colunasXtx, l, c;
+  transposta((float*)x, 34, 100, (float*)tx);
+  multiplica((float*)x, 34, 100, (float*)tx, 100, 34, (float*)xTx, &linhasXtx, &colunasXtx);
+  inversa((float*)xTx, 34, (float*)xTxInv);
+  multiplica((float*)y, 6, 100, (float*)tx, 100, 34, (float*)yTx, &l, &c);
+  multiplica((float*)yTx, 6, 34, (float*)xTxInv, 34, 34, (float*)a, &l, &c);
+}
+void inversa(float *matriz, int ordem, float *resultado){
+	int linha, coluna, k;
+	float pivo, m;
+	identidade(matriz, resultado, ordem);
+
+
+	for(coluna = 0; coluna < ordem; coluna++){
+	    pivo = matriz[coluna*ordem+coluna];
+		for(k = 0; k < ordem; k++){
+			matriz[coluna*ordem+k] = (matriz[coluna*ordem+k])/(pivo); //L1 -> L1/pivo , L2 -> L2/pivo, L3 -> L3/pivo
+			resultado[coluna*ordem+k] = (resultado[coluna*ordem+k])/(pivo); //Ex: 1 0 0 / pivo  , 0 1 0 / pivo,   0 0 1/ pivo
+		}
+
+		for(linha = 0; linha < ordem; linha++){
+			if(linha != coluna){
+				m = matriz[linha*ordem+coluna];
+				for(k = 0; k < ordem; k++){
+					matriz[linha*ordem+k] = (matriz[linha*ordem+k]) - (m*matriz[coluna*ordem+k]); //Ex: L2 -> L2 - ( 1"m" - L1)
+					resultado[linha*ordem+k] = (resultado[linha*ordem+k]) - (m*resultado[coluna*ordem+k] );
+
+				}
+			}
+		}
+	}
+
+
+}
+void transposta(float *matriz, int linhas, int colunas, float *transposta)
+{
+	int i, j;
+	for (i = 0; i < linhas; i++){
+		for (j = 0; j < colunas; j++){
+			transposta[j*linhas+i] = matriz[i*colunas+j];
+		}
+   }
+}
+
+int multiplica(
+		float *a, int linhasA, int colunasA,
+		float *b, int linhasB, int colunasB,
+		float *resultado, int *linhasR, int *colunasR)
+{
+	int i, j, k;//Variáveis para controle de laços.
+	if(colunasA != linhasB){
+		printf("Erro na multiplicação: Matrizes incompatíveis para esta operação.\t\n");
+		return 0;
+	}
+	*colunasR = colunasB;
+	*linhasR = linhasA;
+	for(i = 0; i < linhasA; i++){
+		for(j = 0; j < colunasB; j++){
+			resultado[i*colunasB+j] = 0.0;
+			for(k = 0; k < colunasA; k++){
+				resultado[i*(colunasB)+j] += a[i*colunasA+k]*b[k*colunasB+j];
+			}
+		}
+	}
+	return 1;
+}
+
+
+void identidade(float *matriz, float *resultado, int ordem){
+	int i, j;
+	for (i = 0; i < ordem; i++) {
+		for (j = 0; j < ordem; j++) {
+			if (i == j)
+			{
+				resultado[i*ordem+j] = 1;
+			} else {
+				resultado[i*ordem+j] = 0;
+			}
+		}
+	}
+}
+void mostraMatriz(float *matriz, int linhas, int colunas){
+	int i, j;
+	for(i = 0; i < linhas; i++){
+		for(j = 0; j < colunas; j++){
+        Serial.print(matriz[i*colunas+j]);
+        Serial.print("\t");
+		}
+		Serial.print("\n");
+	}
+}
+
+void exibirDiagnostico(float *matriz) {
+	int linhas = 6;
+	int colunas = 1;
+	int i, j, index = 0;
+	float maior = 0.0;
+	for (i = 0; i < linhas; i++) {
+		for (j = 0; j < colunas; j++) {
+			if (matriz[i * colunas + j] > maior) {
+				maior = matriz[i * colunas + j];
+				index = i;
+			}
+		}
+	}
+	switch (index) {
+	case 0:
+		Serial.println("Psoriase");
+		break;
+	case 1:
+		Serial.println("Dermatite seborreica");
+		break;
+	case 2:
+		Serial.println("Liquen Plano");
+		break;
+	case 3:
+		Serial.println("Pititríase rosea");
+		break;
+	case 4:
+		Serial.println("Dermatite cronica");
+		break;
+	case 5:
+		Serial.println("Pitiriase rubra pilar");
+		break;
+	}
+}
